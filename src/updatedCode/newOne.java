aae81d1ccf0a90e8c69983e4e26a474b2465fd82
pavaniuriti2000 <pavani.uriti123@gmail.com>
2022-12-26 11:37:10 +0530

    My second commit

---
+++ src/updatedCode/newOne.java
@@ -0,0 +1,33 @@
+package updatedCode;
+
+public class newOne {
+	 public static void main(String[] args)
+	    {
+	        // Declaring and initializing integer variable
+		  
+		 //new changes
+		 
+	        int num = 10;
+	 
+	        // Checking if number is even or odd number
+	        // via remainder
+	        if (num % 2 == 0) {
+	 
+	            // If remainder is zero then this number is even
+	            System.out.println("Entered Number is Even");
+	        }
+	 
+	        else {
+	 
+	            // If remainder is not zero then this number is
+	        	//abc
+	            // odd
+	            System.out.println("Entered Number is Odd");
+	        }
+	    }
+	 public void abc()
+	 {
+		 System.out.println("Abc");
+	 }
+
+}
